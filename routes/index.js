8cff29b135e5705d686b9b8ddab24663c804745b
Graham Ballantyne <hello@grahamballantyne.com>
2014-12-08 20:42:18 -0800

    make index route list all OOSes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  models.Staff.findAll({
+      include: [ models.Program ]
+    }).success(function(staff) {
+      res.render('index', {
+        title: 'Express',
+        staff: staff
+      });
+    });
 });
 
 module.exports = router;
