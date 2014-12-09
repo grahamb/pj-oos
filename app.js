ad9ac6b86d6f1745d7c4f0c17f102c201aa3578d
Graham Ballantyne <hello@grahamballantyne.com>
2014-12-08 21:03:49 -0800

    use express-handlebars

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var routes = require('./routes/index');
 var oos = require('./routes/oos');
@@ -11,7 +12,10 @@ var oos = require('./routes/oos');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
