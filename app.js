3c98cdac21b5d567af40e4199c43cc361c353d8f
Graham Ballantyne <hello@grahamballantyne.com>
2014-12-09 23:03:37 -0800

    modify helpers

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ app.engine('hbs', exphbs({
     partialsDir: 'views/partials',
     helpers: {
         selected: function(option, value) {
-            console.log("OPTION", option);
-            console.log("VALUE", value);
+            // console.log("OPTION", option);
+            // console.log("VALUE", value);
             return option === value ? 'selected' : '';
         },
         whatis: function(value) {
-            console.log('---');
+            // console.log('vvv');
             console.dir(value);
-            console.log('---');
+            // console.log('^^^');
         }
     }
 }));
