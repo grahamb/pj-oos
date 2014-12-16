542899b2d8437b03c0f050f5e3e01333d1676cbf
Graham Ballantyne <grahamb@sfu.ca>
2014-12-15 22:57:34 -0800

    add programs route file

---
+++ routes/programs.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+var QueryChainer = require('sequelize').Utils.QueryChainer;
+
+router.get('/', function(req, res) {
+
+});
+
+module.exports = router;
