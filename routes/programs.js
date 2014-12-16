7fb8a8856ee33c0a6dd1c2bbcf2af46b4bda2c3f
Graham Ballantyne <grahamb@sfu.ca>
2014-12-16 00:30:06 -0800

    add program routes

--- routes/programs.js
+++ routes/programs.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
+var Program = require('../models').Program;
 var QueryChainer = require('sequelize').Utils.QueryChainer;
 
 router.get('/', function(req, res) {
+    Program.findAll().then(function(programs) {
+        res.render('programs/index', {
+            title: 'PJ 2015 Programs',
+            programs: programs
+        });
+    });
+});
 
+router.get('/:id', function(req, res) {
+    Program.find({
+        where: { id: req.params.id }
+    }).then(function(program) {
+        res.render('programs/program', {
+            title: 'PJ 2015 Programs - ' + program.name,
+            program: program
+        });
+    });
 });
 
 module.exports = router;
