2cb5c3a7e54ef3ec2a99054859d6c065ac4b7dd8
Graham Ballantyne <hello@grahamballantyne.com>
2014-12-22 14:49:46 -0800

    main route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('hello');
+    res.send('hello');
 });
 
 module.exports = router;
