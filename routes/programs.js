6ac3535dffe7e945ddd3f799f2f1c50cf55d19e6
Graham Ballantyne <grahamb@sfu.ca>
2014-12-23 20:49:38 -0800

    refactor program routes

--- routes/programs.js
+++ routes/programs.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var Program = require('../models').Program;
+var models = require('../models');
+var Program = models.Program, OOS = models.OOS;
 var QueryChainer = require('sequelize').Utils.QueryChainer;
 
 router.get('/', function(req, res) {
-    Program.findAll({ order: 'id ASC', where: { hidden: false }}).then(function(programs) {
+    Program.findAll({ order: 'id ASC', where: { hidden: false }, include: [{model: OOS, as: 'OOS'}] }).then(function(programs) {
         res.render('programs/index', {
             title: 'PJ 2015 Programs',
             programs: programs
@@ -14,7 +15,8 @@ router.get('/', function(req, res) {
 
 router.get('/:id', function(req, res) {
     Program.find({
-        where: { id: req.params.id }
+        where: { id: req.params.id },
+        include: [{model: OOS, as: 'OOS'}]
     }).then(function(program) {
         res.render('programs/program', {
             title: 'PJ 2015 Programs - ' + program.name,
