a8cbf21e3e47d59109d7681dceabb10968e743b3
Graham Ballantyne <grahamb@sfu.ca>
2014-12-26 18:07:48 -0800

    add login routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var passwordless = require('passwordless');
+var requestTokenFn = require('../lib/passwordless/requestToken');
 
 router.get('/', function(req, res) {
     res.send('hello');
 });
+
+router.get('/login', function(req, res) {
+    console.log(req.flash);
+    res.render('login', { auth_message: req.flash('passwordless') });
+});
+
+/* POST login details. */
+router.post('/sendtoken',
+    passwordless.requestToken(requestTokenFn, { failureRedirect: '/login', failureFlash: 'This user is unknown!' }),
+        function(req, res) {
+            // success!
+        res.send('sent');
+});
+
 
 module.exports = router;
