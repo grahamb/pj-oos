2fd0f54c09237f83c122f5ee2fc0e05ee677e013
Graham Ballantyne <hello@grahamballantyne.com>
2015-01-03 01:00:57 -0800

    add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var refills = require('node-refills').includePaths;
-
-var paths = {
-    scss: './public/sass/*.scss'
-};
+var livereload = require('gulp-livereload');
 
 gulp.task('styles', function () {
-    return gulp.src(paths.scss)
+    return gulp.src('./public/sass/style.scss')
         .pipe(sourcemaps.init())
-        .pipe(watch(paths.scss))
-         .pipe(sass({
+        .pipe(sass({
              includePaths: ['styles'].concat(refills),
-             errLogToConsole: true
+             // errLogToConsole: true
          }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
+});
+
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('./public/sass/**/*.scss', ['styles']);
+    gulp.watch(['./views/**/*.hbs']).on('change', function(file) {
+        livereload.changed(file);
+    });
 });
 
 gulp.task('default',function(){
