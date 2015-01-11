54399646df25603c46c66236db02b65431681e57
Graham Ballantyne <grahamb@sfu.ca>
2015-01-10 17:28:19 -0800

    move helpers into their own file

--- app.js
+++ app.js
@@ -31,45 +31,7 @@ app.engine('hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs',
     partialsDir: 'views/partials',
-    helpers: {
-        selected: function(option, value) {
-
-            return option === value ? 'selected' : '';
-        },
-        whatis: function(value) {
-            console.dir(value);
-        },
-        starred_activity: function(program, badge) {
-
-            if (program.premium_activity === false) {
-                return '';
-            }
-
-            var star_icon = '<i class="fa fa-star"></i>';
-            if (badge === false) {
-                return star_icon;
-            }
-
-            return html = '<div class="premium_activity_badge"><span class="badge" style="float:right">' + star_icon + '</span></div>';
-        },
-        list_or_none: function(context) {
-            var arr = [];
-            context = context || [];
-            context.forEach(function(item) {
-                arr.push('<li>' + item + '</li>');
-            });
-            return arr.length ? arr.join('') : '<li>None</li>';
-        },
-        user_can: function(action, options) {
-            var role = options.data.root.role;
-            if (role.can(action)) {
-                console.log('yup')
-                return options.fn(this);
-            } else {
-                console.log('nope');
-            }
-        }
-    }
+    helpers: require('./lib/helpers')
 }));
 app.set('view engine', 'hbs');
 
