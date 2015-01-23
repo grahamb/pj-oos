922f2f1791cc5862801730d290c9acd752866e40
Graham Ballantyne <grahamb@sfu.ca>
2015-01-23 00:41:21 -0800

    use promises to do import

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
-var Import = models.Import;
-var fs = require('fs');
+var Import = models.Import, OOS = models.OOS, Program = models.Program;
 var path = require('path');
+var csv = require('csv');
+var Promise = require('sequelize').Promise;
+var fs = require('fs');
 
 role = require('connect-acl')(require('../lib/roles'));
+
+function parse_csv_file(file) {
+    return new Promise(function (resolve, reject) {
+        var parser = csv.parse({delimiter: ',', columns: true, autoParse: true},
+            function (err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+                parser.end();
+            });
+        fs.createReadStream(file).pipe(parser);
+    });
+}
 
 router.get('/import/oos', role.can('import oos'), function(req, res) {
     res.render('admin/import/oos_import', {
@@ -13,9 +30,9 @@ router.get('/import/oos', role.can('import oos'), function(req, res) {
     });
 });
 
+
+// this is super gross but whatever, it works (I hope)
 router.post('/import/oos', role.can('import oos'), function(req, res) {
-    console.log(req.body) // form fields
-    console.log(req.files)
     Import.create({
         import_type: 'oos',
         status: 'new',
@@ -24,12 +41,49 @@ router.post('/import/oos', role.can('import oos'), function(req, res) {
         mimetype: req.files.import_file.mimetype,
         extension: req.files.import_file.extension,
         size: req.files.import_file.size
-    }).success(function(result) {
-        console.log(result);
-        var id = result.id;
-        res.redirect('/admin/import/oos/' + result.id);
-    });
-});
+    }).then(function(import_record) {
+        var import_id = import_record.id;
+        var import_path = import_record.path;
+
+        parse_csv_file(import_path).then(function(csv_records) {
+            var fields = Object.keys(csv_records);
+            ['assigned_department', 'desired_assignment'].forEach(function(field) {
+                fields.splice(fields.indexOf(field), 1);
+            });
+
+            var import_records = csv_records.map(function(record) {
+                record.notes = record.assigned_department + '\n' + record.desired_assignment;
+                record.import_id = import_id;
+                return record;
+            });
+
+            return Promise.map(import_records, function(record) {
+
+                return OOS.findOrCreate({
+                    where: { oos_number: record.oos_number },
+                    fields: fields,
+                    defaults: record,
+                    include: [Program]
+                });
+
+            }).spread(function() {
+
+                // args is an array of [{record}, created]
+                var args = Array.prototype.slice.call(arguments);
+
+                var created_records = [];
+                args.forEach(function(arr) {
+                    var record = arr[0], created = arr[1];
+                    if (created) {
+                        created_records.push(record);
+                    }
+                });
+                return Promise.map(created_records , function(record) {
+                    return record.setPrograms([0]);
+                });
+            }).then(function() {
+                res.redirect('/oos');
+            });
 
         });
 
