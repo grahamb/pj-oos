e50ef80cf8f3892d4044cd77f91033a6bfe9b06a
Graham Ballantyne <grahamb@sfu.ca>
2015-02-26 16:03:04 -0800

    better errors

--- app.js
+++ app.js
@@ -113,25 +113,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        if (err.status === 404) {
-            res.render(404);
-        } else {
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
-        }
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     if (err.status === 404) {
