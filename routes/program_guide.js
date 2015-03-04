c73d24e8d4e076a3259e3937580b5d7bd4478dac
Graham Ballantyne <grahamb@sfu.ca>
2015-03-03 16:19:27 -0800

    add program listing to program guide

--- routes/program_guide.js
+++ routes/program_guide.js
@@ -6,9 +6,17 @@ var Promise = require('sequelize').Promise;
 var role = require('connect-acl')(require('../lib/roles'));
 
 router.get('/', function(req, res) {
+  Program.findAll({
+    where: {
+      hidden: false
+    },
+    order: 'location ASC'
+  }).then(function(programs) {
     res.render('program_guide/index', {
-        title: '- Program Guide'
+        title: '- Program Guide',
+        programs: programs
     });
+  })
 });
 
 module.exports = router;
