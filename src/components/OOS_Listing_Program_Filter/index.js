1b693ea91e4cec87f14bf7fd9118c0c323e91f87
Graham Ballantyne <grahamb@sfu.ca>
2015-03-04 11:29:00 -0800

    add OOS_Listing_Program_Filter component

---
+++ src/components/OOS_Listing_Program_Filter/index.js
@@ -0,0 +1,15 @@
+var $ = require('jquery');
+var getQueryVariable = require('getQueryVariable');
+
+var filter = getQueryVariable('program');
+if (filter) {
+  $('.assignmentFilter [value="' + filter + '"]').attr('selected', true);
+  $('a.csv_export').attr('href', '/oos/csv?program=' + filter);
+}
+
+$('.assignmentFilter').on('change', function(el) {
+  var val = $(this).val();
+  var location = '/oos';
+  window.location = val.toLowerCase() === 'all' ? location : location + '?program=' + val;
+});
+
