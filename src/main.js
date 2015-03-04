757c94dc13b8dfef60b41961fb7170233c7d6942
Graham Ballantyne <grahamb@sfu.ca>
2015-03-03 20:31:42 -0800

    add development mode banner

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+require('Navbar')();
+
 if (__PRODUCTION__) {
   require('./ga');
+} else {
+  console.log("       d8b                         d8b                                                           \n       88P                         88P                                                     d8P   \n      d88                         d88                                                   d888888P \n  d888888   d8888b?88   d8P d8888b888   d8888b ?88,.d88b,  88bd8b,d88b  d8888b  88bd88b   ?88'   \n d8P' ?88  d8b_,dPd88  d8P'd8b_,dP?88  d8P' ?88`?88'  ?88  88P'`?8P'?8bd8b_,dP  88P' ?8b  88P    \n 88b  ,88b 88b    ?8b ,88' 88b     88b 88b  d88  88b  d8P d88  d88  88P88b     d88   88P  88b    \n `?88P'`88b`?888P'`?888P'  `?888P'  88b`?8888P'  888888P'd88' d88'  88b`?888P'd88'   88b  `?8b   \n                                                 88P'                                            \n                                                d88                                              \n                                                ?8P                                              \n")
 }
-require('Navbar')();
-if (!__PRODUCTION__) { console.log('this is not production'); }
