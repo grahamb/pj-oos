d86262df7696534ff75aeecc92c5657be18ed688
Graham Ballantyne <grahamb@sfu.ca>
2015-03-03 16:18:44 -0800

    gulpfile indent

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,32 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var livereload = require('gulp-livereload');
 var sourcemaps = require('gulp-sourcemaps');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('sass', function() {
-    return sass('./public/sass/style.scss', { sourcemap: true })
-        .on('error', function (err) {
-            console.error('Error!', err.message);
-        })
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public/css'))
-        .pipe(livereload());
+  return sass('./public/sass/style.scss', { sourcemap: true })
+  .on('error', function (err) {
+    console.error('Error!', err.message);
+  })
+  .pipe(sourcemaps.write())
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('./public/css'))
+  .pipe(livereload());
 });
 
 
 gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('./public/sass/**/*.scss', ['sass']);
-    gulp.watch(['./views/**/*.hbs']).on('change', function(file) {
-        livereload.changed(file);
-    });
+  livereload.listen();
+  gulp.watch('./public/sass/**/*.scss', ['sass']);
+  gulp.watch(['./views/**/*.hbs']).on('change', function(file) {
+    livereload.changed(file);
+  });
 });
 
 gulp.task('default',function(){
-    gulp.start('sass');
+  gulp.start('sass');
 });
+
+gulp.task('dev', function() {
+
+})
