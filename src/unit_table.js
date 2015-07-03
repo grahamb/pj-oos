f0a013784b666e7c63b9f2ce3b9ca6ac66ff9382
Graham Ballantyne <grahamb@sfu.ca>
2015-07-03 10:04:42 -0700

    add unit_table

---
+++ src/unit_table.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import React from 'react';
+import UnitTable from 'UnitTable';
+
+React.render(<UnitTable tableHeight={600} tableWidth={1087} />, document.getElementById('unittable'));
